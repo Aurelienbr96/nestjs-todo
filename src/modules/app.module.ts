c222302c1d2133e41fad244456611865201edb6d
Aurelienbr96 <aurelien.brachet@prium-solutions.com>
2024-04-04 15:16:41 +0200

    fix: e2e testing for automation

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user';
 import { AuthModule } from './auth';
 import { MuscleGroupModule } from './muscleGroup';
 import { ExerciseModule } from './exercise';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CommonModule, UserModule, AuthModule, MuscleGroupModule, ExerciseModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
